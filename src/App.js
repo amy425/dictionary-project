302dc189f5c4288d61ee629bff9489bb11af081c
amysteel <75953169+amysteel@users.noreply.github.com>
2021-07-10 17:09:58 +0100

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         Dictionary
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Amy</footer>
+      </div>
     </div>
   );
 }
